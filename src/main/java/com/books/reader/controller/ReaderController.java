c814661b511f7f0ec9faee62f6666f6986bbcecd
Julia Lumbansiantar <jflumbansiantar@gmail.com>
2022-08-24 21:44:30 +0700

    fixing repo&service

--- src/main/java/com/books/reader/controller/ReaderController.java
+++ src/main/java/com/books/reader/controller/ReaderController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.*;
 
 import com.books.reader.dto.ReaderDto.*;
 import com.books.reader.service.ReaderService;
-import javax.servlet.http.HttpServletRequest;
+
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-
 
 @Slf4j
 @RestController
@@ -21,46 +20,38 @@ public class ReaderController {
     
     @SneakyThrows(Exception.class)
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Object> getReader(@PathVariable(value="id") Integer id){
-        log.info("GET http://localhost:8099/api/v1/reader/{} is called", id);
-        
+    public ResponseEntity<Object> getReader(@PathVariable(value="id") String id){
+        log.info("GET http://localhost:8080/api/v1/book/{} is called", id);
 		return readerService.getReader(id);
     }
 
     //POST
     @SneakyThrows(Exception.class)
     @PostMapping(value = "/add")
-    // @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Object> createReader(HttpServletRequest request, @RequestBody CreateReader dto){
-        log.info("POST http://localhost:8099/api/v1/reader is called");
-        boolean result = CheckToken.checkAuthorization(request, "SUPER_ADMIN");
-        return readerService.createReader(result, dto);
+    public ResponseEntity<Object> createReader(@RequestBody CreateReader dto){
+        log.info("POST http://localhost:8080/api/v1/book is called");
+        return readerService.createReader(dto);
     }
 
     //PUT
     @SneakyThrows(Exception.class)
-    @PutMapping(value = "/{id_reader}")
-    // @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Object> updateReader(HttpServletRequest request, @RequestBody UpdateReader dto){
-        log.info("PUT http://localhost:8099/api/v1/reader is called");
-        boolean result = CheckToken.checkAuthorization(request, "SUPER_ADMIN");
-        return readerService.updateReader(result, dto);
+    @PutMapping(value = "/{id_book}")
+    public ResponseEntity<Object> updateReader(@PathVariable("id") String id, @RequestBody UpdateReader dto){
+        log.info("PUT http://localhost:8080/api/v1/book is called");
+        return readerService.updateReader(id, dto);
     }
     //DELETE
     @SneakyThrows(Exception.class)
-    @DeleteMapping(value = "/{id_reader}")
-    // @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Object> deleteReader(HttpServletRequest request, @PathVariable(name = "id") Integer id){
-        log.info("DELETE http://localhost:8099/api/v1/reader is called");
-        boolean result = CheckToken.checkAuthorization(request, "SUPER_ADMIN");
-        return readerService.deleteReader(result, id);
+    @DeleteMapping(value = "/{id_book}")
+    public ResponseEntity<Object> deleteReader(@PathVariable(name = "id") String id){
+        log.info("DELETE http://localhost:8080/api/v1/book is called");
+        return readerService.deleteReader(id);
     }
     //GETALL
     @SneakyThrows(Exception.class)
     @GetMapping(value = "/")
-    // @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Object> getReaders(){
-        log.info("GET http://localhost:8099/api/v1/reader/ is called");
+        log.info("GET http://localhost:8080/api/v1/book/ is called");
 		return readerService.getReaders();
     }
 }
--- src/main/java/com/books/reader/service/ReaderService.java
+++ src/main/java/com/books/reader/service/ReaderService.java
@@ -4,6 +4,7 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.SneakyThrows;
 import lombok.val;
@@ -19,42 +20,40 @@ public class ReaderService {
 
     //create reader
     @SneakyThrows(Exception.class)
-    public ResponseEntity<Object> createReader(boolean auth, CreateReader dto){
+    public ResponseEntity<Object> createReader(CreateReader dto){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+
         Map<String, Object> res = new HashMap<String, Object>();
-        if (auth==true){
-            Reader reader = new Reader();
-            
-            reader.setReader_name(dto.getReader_name());
-            reader.setAddress(dto.getAddress());
-            reader.setEmail(dto.getEmail());
-            reader.setGender(dto.getGender());
-            reader.setPhone(dto.getPhone());
-            reader.setIs_borrower(dto.getIs_borrower());
-            reader.setIs_deleted(0);
 
-            readerRepository.save(reader);
+        Reader reader = new Reader();
+        
+        reader.setReader_name(dto.getReader_name());
+        reader.setAddress(dto.getAddress());
+        reader.setEmail(dto.getEmail());
+        reader.setGender(dto.getGender());
+        reader.setPhone(dto.getPhone());
+        reader.setIs_borrower(dto.getIs_borrower());
+        reader.setIs_deleted(0);
 
-            res.put("code", HttpStatus.CREATED.value());
-            res.put("message", "success");
-            res.put("data", reader);
-        } else {
-            res.put("message", "failed");
-			res.put("data", null);
-        }
+        readerRepository.save(reader);
+
+        res.put("code", HttpStatus.CREATED.value());
+        res.put("message", "success");
+		res.put("data", reader);
+
         return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(res);
     }
     //update reader
-    public ResponseEntity<Object> updateReader(boolean auth, UpdateReader dto){
+    public ResponseEntity<Object> updateReader(String id, UpdateReader dto){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, Object> res = new HashMap<String, Object>();
-    
-        val reader = readerRepository.findById(dto.getId()).orElse(null);
+
+        val reader = readerRepository.findById(dto.get_id()).orElse(null);
         
-        if(Optional.ofNullable(reader).isPresent() && auth==true){
+        if(Optional.ofNullable(reader).isPresent()){
             reader.setReader_name(dto.getReader_name());
             reader.setAddress(dto.getAddress());
             reader.setEmail(dto.getEmail());
@@ -75,7 +74,7 @@ public class ReaderService {
         return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(res);
     }
     //delete reader
-    public ResponseEntity<Object> deleteReader(boolean auth, Integer id){
+    public ResponseEntity<Object> deleteReader(String id){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -83,7 +82,7 @@ public class ReaderService {
 
         val reader = readerRepository.findById(id).orElse(null);
         
-        if(Optional.ofNullable(reader).isPresent() && auth==true){
+        if(Optional.ofNullable(reader).isPresent()){
             reader.setIs_deleted(1);
             reader.setDeleted_at(new Date());
 
@@ -118,14 +117,14 @@ public class ReaderService {
 	}
 
     @SneakyThrows(Exception.class)
-	public ResponseEntity<Object> getReader(Integer id) {
+	public ResponseEntity<Object> getReader(@PathVariable("id") String id) {
 		
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
+        Map<String, Object> res = new HashMap<String, Object>();
+		Optional<Reader> reader = readerRepository.findById(id);
 
-		Map<String, Object> res = new HashMap<String, Object>();
-
-		val reader = readerRepository.findById(id);
+		
 
 		res.put("code", HttpStatus.OK.value());
 		res.put("message", "success");
